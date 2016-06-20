9fd1bc7c2f42bac2e285c2cf759833b56a3dfe6a
MarkPerez <markperez88@hotmail.co.uk>
2016-06-20 11:43:53 +0100

    GuessTheNumber

---
+++ GuessTheNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random genRan = new Random();
+            int secretNumber = genRan.Next(1, 11);
+            int numberGuess;
+            do
+            {
+                Console.WriteLine("Can you guess the number between 1 and 10? Enter your guess below:");
+                numberGuess = Convert.ToInt32(Console.ReadLine());
+                if (numberGuess < secretNumber)
+                {
+                    Console.WriteLine("Your guess is to low!");
+                }
+                else if (numberGuess > secretNumber)
+                {
+                    Console.WriteLine("Your guess is too high!");
+                }
+            }
+            while (numberGuess != secretNumber);
+            Console.WriteLine("Correct!");
+        }
+    }
+}
